3f08bfb1014b6478b6686d0445d0f88b8425a616
Jessy Vandoorn <jessy.vandoorn@student.howest.be>
2018-02-14 19:35:36 +0100

    adding some css style lines

--- src/events.json.php
+++ src/events.json.php
@@ -5,7 +5,7 @@
 			"id": "0",
 			"title": "FauxFête",
 			"url": "https://www.facebook.com/events/1715554375162284/",
-			"class": "event-warning",
+			"class": "event-info",
 			"start": "1518829200000",
 			"end":   "1518915600000"
 		},
@@ -13,7 +13,7 @@
 			"id": "1",
 			"title": "Vriendjes- & Familiecantus",
 			"url": "http://www.thraso.com/",
-			"class": "event-info",
+			"class": "event-warning",
 			"start": "1518224400000",
 			"end":   "1518310800000"
 		},
